2169010a345d3e545a14d3dfb2b9737047d5357c
Jafet Guzman <jafetguzmanflores@gmail.com>
2022-11-23 21:58:37 -0500

    added a working config file
    running cloudinary, mongoDb, and whatever package.json has. However,
    had to hard-code the cloudinary config bc env didnt like it

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,11 +1,12 @@
 const cloudinary = require("cloudinary").v2;
 
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "../config/.env" });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+cloud_name: 'duszgpq96', 
+api_key: '976782784619354', 
+api_secret: '9pMeNcrQeeyJLkf8ph0IWV_toXc' 
 });
+
 
 module.exports = cloudinary;
